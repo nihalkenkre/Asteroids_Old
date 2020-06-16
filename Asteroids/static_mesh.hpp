c384c6e65dd5d8df42109e201a579b2b2406f54d
Nihal Kenkre <nihal.kenkre@gmail.com>
2020-06-17 00:32:36 +0530

    added big five for static_mesh

--- Asteroids/static_mesh.hpp
+++ Asteroids/static_mesh.hpp
@@ -11,9 +11,12 @@
 class static_mesh
 {
 public:
-    static_mesh (const static_mesh& other) { this->name = other.name; }
+    static_mesh (const static_mesh& other);
+    static_mesh& operator= (const static_mesh& other); 
+    static_mesh (const static_mesh&& other) noexcept;
+    static_mesh& operator= (const static_mesh&& other) noexcept;
     static_mesh (const tinygltf::Node& graphics_node, const std::vector<tinygltf::Node>& physics_nodes, const tinygltf::Model& model);
-    ~static_mesh ();
+    ~static_mesh () noexcept;
     
     std::string name;
 
